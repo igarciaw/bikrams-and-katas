1f674aeeeb4327bacfb4b443fbc940dcd2423c3f
Irene García <irenegwodak@gmail.com>
2025-01-29 01:01:38 +0100

    feat(chuck-list-joke): add "delete all" button with dynamic visibility
    Added a "delete all" button in the HTML, initially hidden using the 'hidden' class.
    The button becomes visible when at least one joke is present and hides when there are no jokes.
    Styled the button for better appearance.

--- 05-chuckListJoke/js/script.js
+++ 05-chuckListJoke/js/script.js
@@ -2,6 +2,7 @@
 
 const btn = document.querySelector('.js-btn');
 const jokeListContainer = document.querySelector('.js-joke-list');
+const deleteAllBtn = document.querySelector('.js-delete-all-btn');
 let jokeListItems = JSON.parse(localStorage.getItem('jokeList')) || [];
 
 function getJoke() {
@@ -9,12 +10,14 @@ function getJoke() {
     .then((response) => response.json())
     .then((data) => {
       jokeListItems.push(data.value);
-      localStorage.setItem('jokeList', JSON.stringify(jokeListItems));
-      printJokeList();
+      setLSAndprint();
     })
     .catch((error) => {
       console.error('Error en la solicitud:', error);
     });
+}
+function setLS() {
+  localStorage.setItem('jokeList', JSON.stringify(jokeListItems));
 }
 function printJokeList() {
   jokeListContainer.innerHTML = '';
@@ -27,18 +30,32 @@ function printJokeList() {
       `;
   });
 }
+function printDeleteAllBtn() {
+  if (jokeListItems.length > 0) {
+    deleteAllBtn.classList.remove('hidden');
+  } else {
+    deleteAllBtn.classList.add('hidden');
+  }
+}
+function setLSAndprint() {
+  setLS();
+  printJokeList();
+  printDeleteAllBtn();
+}
 
 function deleteJoke(ev) {
   console.log(ev.target.id);
   jokeListItems.forEach((joke, i) => {
     if (i == ev.target.id) {
       jokeListItems.splice(i, 1);
-      localStorage.setItem('jokeList', JSON.stringify(jokeListItems));
-      printJokeList();
+      setLSAndprint();
     }
   });
 }
 btn.addEventListener('click', getJoke);
 jokeListContainer.addEventListener('click', deleteJoke);
-
+deleteAllBtn.addEventListener('click', () => {
+  jokeListItems = [];
+  setLSAndprint();
+});
 printJokeList();
