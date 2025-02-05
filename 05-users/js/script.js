bb67641f202ebe5eb073e9e1ae20e616defe2faf
Irene García <irenegwodak@gmail.com>
2025-02-05 13:18:15 +0100

    style(05-users): add styles for user data display

--- 05-users/js/script.js
+++ 05-users/js/script.js
@@ -16,17 +16,21 @@ function randomAge() {
 function renderUsers(users) {
   users.forEach(({ id, name, username, email, address, phone, company }) => {
     htmlUserList.innerHTML += `
-        <li>
-            <img src="./assets/img/${id}.jpeg" alt="Foto de perfil ID="${id}" />
-            <p>Nombre: ${name}</p>
-            <p>Edad: ${randomAge()}</p>
-            <p>Username: ${username}</p>
-            <p>Teléfono: ${phone}</p>
-            <p>Email: ${email}</p>
-            <p>Compañía: ${company.name}</p>
-            <p>Dirección: ${address.street}, ${address.suite}, ${
-      address.city
-    }</p>
+        <li class="user-container">
+            <div class="user-info-container">
+                <ul class="user-data-container">
+                    <li><strong>Nombre:</strong> ${name}</li>
+                    <li><strong>Edad:</strong> ${randomAge()}</li>
+                    <li><strong>Username:</strong> ${username}</li>
+                    <li><strong>Teléfono:</strong> ${phone}</li>
+                    <li><strong>Email:</strong> ${email}</li>
+                </ul>
+                    <img src="./assets/img/${id}.jpeg" alt="Foto de perfil ID="${id}" />
+            </div>
+            <ul class="company-info-container">
+                <li><strong>Compañía:</strong> ${company.name}</li>
+                <li><strong>Dirección:</strong> ${address.street}, ${address.suite}, ${address.city}<li>
+            </ul>
       </li>
         `;
   });
