180f8e32e039c3068f467d4664b87055d69e4a29
Irene García <irenegwodak@gmail.com>
2025-02-11 13:13:19 +0100

    feat(00-Stackblitz): solve s06 asyn-await exercise

---
+++ 00-Stackblitz/04-JS-adv/s06-02-async-await/script.js
@@ -0,0 +1,29 @@
+async function getUserData(userID) {
+  const url = `https://jsonplaceholder.typicode.com/users/${userID}`;
+
+  const userData = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const user = {
+        name: data.name,
+        email: data.email,
+        city: data.address.city,
+      };
+      return user;
+    })
+    .catch((error) => {
+      console.error('Error en la solicitud:', error);
+    });
+
+  return userData;
+}
+
+getUserData(1).then(console.log);
+
+Promise.all([getUserData(1), getUserData(2), getUserData(3)]).then((values) => {
+  values.forEach((value) =>
+    console.log(
+      `Nombre: ${value.name}, Email: ${value.email}, Ciudad: ${value.city}`
+    )
+  );
+});
